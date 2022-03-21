5e1abd19ff4980e4a6558a70825523b683146c06
Chinmay Kulkarni <chinmayrk01@gmail.com>
2022-03-21 16:49:54 +0530

    fixed bugs

--- app/src/main/java/com/chinmay/horobook/view/SongsListFragment/SongsListAdapter.kt
+++ app/src/main/java/com/chinmay/horobook/view/SongsListFragment/SongsListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.chinmay.horobook.R
@@ -39,22 +40,46 @@ class SongsListAdapter(val songsList: ArrayList<SongsListData>) :
         holder.view.name.text = songsList[position].songName
         holder.view.lifespan.text = songsList[position].songArtist
         holder.view.setOnClickListener {
-            Navigation.findNavController(it)
-                .navigate(
-                    SongsListFragmentDirections.actionPlayerFragment(
-                        songsList[position].songUrl.toString(),
-                        songsList[position].songImageUrl.toString(),
-                        songsList[position].songName.toString()
+            if (songsList[position].playSong == true) {
+                Navigation.findNavController(it)
+                    .navigate(
+                        SongsListFragmentDirections.actionPlayerFragment(
+                            songsList[position].songUrl.toString(),
+                            songsList[position].songImageUrl.toString(),
+                            songsList[position].songName.toString()
+                        )
                     )
-                )
-            holder.view.list_clickable.isClickable = false
-
+                holder.view.list_clickable.isClickable = false
+            } else {
+                showDialog("Sorry!!", "You cannot play this song", holder)
+            }
 
         }
         holder.view.imageView.loadImage(
             UrlConstants.media_url + songsList[position].songImageUrl,
             getProgressDrawable(holder.view.imageView.context)
         )
+    }
+
+
+
+    private fun showDialog(title: String, msg: String, holder: SongsListAdapter.SongViewHolder) {
+        val builder = AlertDialog.Builder(holder.view.context)
+        //set title for alert dialog
+        builder.setTitle(title)
+        //set message for alert dialog
+        builder.setMessage(msg)
+        //builder.setIcon(android.R.drawable.ic_input_add)
+
+        //performing positive action
+        builder.setPositiveButton("OK") { dialogInterface, which ->
+
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 
     private fun playAudio(holder: SongViewHolder) {
